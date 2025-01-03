885c2fac291ea64f6bc89ca8726ac3e0490c0ff2
Bastien Lacave <bastien.lacave@unige.ch>
2025-01-03 16:45:28 +0100

    Add unit test for version check and comment out pre-commit action in CI configuration

---
+++ src/ctlearn_manager/tests/test_version.py
@@ -0,0 +1,10 @@
+import unittest
+import ctlearn_manager
+
+class TestVersion(unittest.TestCase):
+    def test_version(self):
+        expected_version = "1.0.0"  # Replace with the expected version
+        self.assertEqual(ctlearn_manager.__version__, expected_version)
+
+if __name__ == '__main__':
+    unittest.main()
