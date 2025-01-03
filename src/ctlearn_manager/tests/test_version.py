3cb52015430b54cc4e76ddf6115ee0e1847720bb
Bastien Lacave <bastien.lacave@unige.ch>
2025-01-03 16:50:15 +0100

    Update expected version in version test to match current release

--- src/ctlearn_manager/tests/test_version.py
+++ src/ctlearn_manager/tests/test_version.py
@@ -3,7 +3,7 @@ import ctlearn_manager
 
 class TestVersion(unittest.TestCase):
     def test_version(self):
-        expected_version = "0.0.0"  # Replace with the expected version
+        expected_version = "0.1.dev1+g4ad0be7"  # Replace with the expected version
         self.assertEqual(ctlearn_manager.__version__, expected_version)
 
 if __name__ == '__main__':
