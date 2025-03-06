f59dfcc9894f8d9d4e49045a5e3873c493aede67
Bastien Lacave <bastien.lacave@cta-consortium.org>
2025-03-06 13:13:57 +0000

    more flexible loading if fields are missing, dynamic keys

--- src/ctlearn_manager/io/io.py
+++ src/ctlearn_manager/io/io.py
@@ -13,19 +13,50 @@ def load_model_from_index(model_nickname, MODEL_INDEX_FILE):
     return model
 
 
-def load_DL2_data_MC(input_file):
+def load_DL2_data_MC(input_file, tel_id=None):
     from ctapipe.io import read_table
     from astropy.table import (join, hstack)
-    pointing = read_table(input_file, "dl1/monitoring/subarray/pointing/")
-    dl2_classification = read_table(input_file, "dl2/event/subarray/classification/CTLearn")
-    dl2_classification = hstack([dl2_classification, pointing])
-    dl2_classification = dl2_classification[~np.isnan(dl2_classification["CTLearn_prediction"])]
-    dl2_energy = read_table(input_file, "dl2/event/subarray/energy/CTLearn")
-    dl2_energy = dl2_energy[~np.isnan(dl2_energy["CTLearn_energy"])]
-    dl2_geometry = read_table(input_file, "dl2/event/subarray/geometry/CTLearn")
-    dl2_geometry = dl2_geometry[~np.isnan(dl2_geometry["CTLearn_alt"])]
-    dl2 = join(dl2_classification, dl2_energy, keys=["obs_id", "event_id"])
-    dl2 = join(dl2, dl2_geometry, keys=["obs_id", "event_id"])
+    subarray_string = "subarray" if tel_id==None else "telescope"
+    tel_id_string = "" if tel_id==None else f"tel_{tel_id:03d}"
+    pointing = read_table(input_file, f"dl1/monitoring/{subarray_string}/pointing/{tel_id_string}")
+    key_tel = "" if tel_id==None else "tel_"
+
+    dl2_tables = []
+
+    try:
+        dl2_classification = read_table(input_file, f"dl2/event/{subarray_string}/classification/CTLearn/{tel_id_string}")
+        dl2_classification = hstack([dl2_classification, pointing])
+        dl2_classification = dl2_classification[~np.isnan(dl2_classification[f"CTLearn_{key_tel}prediction"])]
+        dl2_tables.append(dl2_classification)
+    except:
+        print(f"Classification table not found for dl2/event/{subarray_string}/classification/CTLearn/{tel_id_string}")
+    
+    try:
+        dl2_energy = read_table(input_file, f"dl2/event/{subarray_string}/energy/CTLearn/{tel_id_string}")
+        if len(dl2_tables) == 0:
+            dl2_energy = hstack([dl2_energy, pointing])
+        dl2_energy = dl2_energy[~np.isnan(dl2_energy[f"CTLearn_{key_tel}energy"])]
+        dl2_tables.append(dl2_energy)
+    except:
+        print(f"Energy table not found for dl2/event/{subarray_string}/energy/CTLearn/{tel_id_string}")
+
+    try:
+        dl2_geometry = read_table(input_file, f"dl2/event/{subarray_string}/geometry/CTLearn/{tel_id_string}")
+        if len(dl2_tables) == 0:
+            dl2_geometry = hstack([dl2_geometry, pointing])
+        dl2_geometry = dl2_geometry[~np.isnan(dl2_geometry[f"CTLearn_{key_tel}alt"])]
+        dl2_tables.append(dl2_geometry)
+    except:
+        print(f"Geometry table not found for dl2/event/{subarray_string}/geometry/CTLearn/{tel_id_string}")
+    # dl2 = join(dl2_classification, dl2_energy, keys=["obs_id", "event_id"])
+    # dl2 = join(dl2, dl2_geometry, keys=["obs_id", "event_id"])
+    if len(dl2_tables) > 0:
+        dl2 = dl2_tables[0]
+        for table in dl2_tables[1:]:
+            dl2 = join(dl2, table, keys=["obs_id", "event_id"])
+    else:
+        raise ValueError("No DL2 tables found")
+
     return dl2
 
 def load_true_shower_parameters(input_file):
